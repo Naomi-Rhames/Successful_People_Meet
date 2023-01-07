93254defdb455edb8460a8b11f6e512460b0e993
Naomi-Rhames <naomirhames2020@gmail.com>
2023-01-07 12:52:19 -0600

    fixed misspelling error in datatable

--- db/migrate/20230106155743_create_profiles.rb
+++ db/migrate/20230106155743_create_profiles.rb
@@ -1,6 +1,6 @@
-class CreateProfile < ActiveRecord::Migration[7.0]
+class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
-    create_table :profile do |t|
+    create_table :profiles do |t|
       t.string :first_name
       t.string :last_name
       t.integer :age
