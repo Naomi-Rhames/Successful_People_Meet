402be34cdd16faf987059d2f28a7567238ab3cfd
Naomi-Rhames <naomirhames2020@gmail.com>
2023-02-15 19:01:27 -0600

    new methods in Profile Controller

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -3,12 +3,23 @@ module Api
         class ProfileController < ApplicationController
 
             def create
-                
+                new_profile = Profile.new(profile_params)
+                if new_profile.save
+                    render json: new_profile
+                else
+                    render json: {error: user.error.messages}, status: 422
+                end 
             end
         
             def index
                 profile = Profile.all
                 render json: profile
+            end
+
+            private
+
+            def profile_params
+                params.permit(:first_name, :last_name, :age, :profile_image, :status, :about_me, :occupation, :hobbies, :location)
             end
 
         end
