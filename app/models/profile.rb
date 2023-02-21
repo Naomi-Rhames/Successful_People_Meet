790f80b58f0c98ed515c246223f06f93b9e7cc7b
Naomi-Rhames <naomirhames2020@gmail.com>
2023-02-20 20:36:39 -0600

    validation in profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,11 @@
 class Profile < ApplicationRecord
     belongs_to :user
+
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :age, presence: true
     validates :status, presence: true
+    validates :location, presence: true
     validates :about_me, presence: true
     validates :occupation, presence: true
     validates :hobbies, presence: true
